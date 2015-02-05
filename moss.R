cc7543bc70f2f538e89dbfa7495285b770b7e72b
urban <anatoly.urban@gmail.com>
2015-02-05 22:32:20 +0300

    Groups.

--- moss.R
+++ moss.R
@@ -1,3 +1,30 @@
+####### Parameters #######
+FC2_TH = 1
+FC15_TH = 1
+TH = FC15_TH
+##########################
+
+group <- function(mrna, prot)
+{
+    if (abs(mrna) > TH)
+        if (mrna > 0)
+            m <- 'M'
+        else
+            m <- 'm'
+    else
+        m <- '0'
+    
+    if (abs(prot) > TH)
+        if (prot > 0)
+            p <- 'P'
+        else
+            p <- 'p'
+    else
+        p <- '0'
+    
+    paste0(m, p)
+}
+
 mrna <- read.table("transcripts.csv", sep = '\t', header = TRUE, stringsAsFactors = FALSE)
 mrna <- data.frame(Gene = mrna$Gene,# TAIR = mrna$TAIR,
                   PP = mrna$Protoplasts_FPKM,
@@ -14,6 +41,15 @@ prot <- prot[grepl("^Pp.+", prot$Gene),]
 prot$Gene <- sapply(strsplit(prot$Gene, split = '\\.'), "[", 1)
 prot <- aggregate(. ~ Gene, data = prot, FUN = sum)
 
-total <- merge(mrna[,c("Gene", "mrnaPNtoPP")], prot, by = "Gene")
+total <- merge(mrna[,c("Gene", "mrnaPNtoPP")], prot, by = "Gene" )#, all.y = TRUE)
 
-plot(total$mrnaPNtoPP, total$protPNtoPP)
+total$mrnafc <- log(total$mrnaPNtoPP, base = 1.5)
+total$protfc <- log(total$protPNtoPP, base = 1.5)
+
+total$group <- as.factor(apply(total[,c("mrnafc", "protfc")], 1,
+                               function(x) group(x[1], x[2])))
+
+total <- total[ abs(total$mrnafc) > TH |
+                abs(total$protfc) > TH,]
+
+plot(total$mrnafc, total$protfc, pch = 19, col = total$group)
