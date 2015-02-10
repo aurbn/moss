d066b95a687eca02b89bc5a64f8231365b157a62
urban <anatoly.urban@gmail.com>
2015-02-10 20:54:32 +0300

    P-values  filtering.

--- moss.R
+++ moss.R
@@ -6,12 +6,12 @@ TH = FC15_TH
 SWATH_SOURCE = "raw"   # "raw" or "processed"
 PN_SAMPLES = c("F163", "F164", "F165")
 PP_SAMPLES = c("F166", "F167", "F168", "F169")
-PROT_FILTER_PV = TRUE
-PROT_REQ_PV = 0.05
+PROT_FILTER_PV = FALSE
+PROT_REQ_PV = 2
 
 ##########################
 
-group <- function(mrna, prot)
+group <- function(mrna, prot, pvmrna, protpv)
 {
     if (abs(mrna) > TH)
         if (mrna > 0)
@@ -21,7 +21,7 @@ group <- function(mrna, prot)
     else
         m <- '0'
     
-    if (abs(prot) > TH)
+    if (abs(prot) > TH & protpv < PROT_REQ_PV)
         if (prot > 0)
             p <- 'P'
         else
@@ -72,6 +72,7 @@ if (SWATH_SOURCE == "processed")
     prot <- read.table("proteins.csv", sep = '\t', header = TRUE, stringsAsFactors = FALSE)
     prot <- data.frame(Gene = prot$ProteinName, protPPtoPN = 1/prot$PNtoPP_1.fc, 
                        stringsAsFactors = FALSE)
+    prot$protpv <- 0
 } else if (SWATH_SOURCE == "raw")
 {
     prot <- read.table("swath_raw.csv", sep = '\t', header = TRUE, stringsAsFactors = FALSE)
@@ -80,10 +81,10 @@ if (SWATH_SOURCE == "processed")
     prot$PN <- apply(prot[,PN_SAMPLES], 1, FUN = mean)
     prot$PP <- apply(prot[,PP_SAMPLES], 1, FUN = mean)
     prot$protPPtoPN <- prot$PP/prot$PN
-    prot$pv <- apply(prot, 1, tst, PN_SAMPLES, PP_SAMPLES)
+    prot$protpv <- apply(prot, 1, tst, PN_SAMPLES, PP_SAMPLES)
     if (PROT_FILTER_PV)
     {
-        prot <- prot[prot$pv < PROT_REQ_PV,]
+        prot <- prot[prot$protpv < PROT_REQ_PV,]
     }
 } else
 {
@@ -95,13 +96,13 @@ prot$Gene <- sapply(strsplit(prot$Gene, split = '\\.'), "[", 1)
 prot <- aggregate(. ~ Gene, data = prot, FUN = sum)
 
 total <- merge(mrna[,c("Gene", "mrnaPPtoPN")], 
-               prot[, c("Gene", "protPPtoPN")], by = "Gene" )#, all.y = TRUE)
+               prot[, c("Gene", "protPPtoPN", "protpv")], by = "Gene" )#, all.y = TRUE)
 
 total$mrnafc <- log(total$mrnaPPtoPN, base = 1.5)
 total$protfc <- log(total$protPPtoPN, base = 1.5)
 
-total$group <- as.factor(apply(total[,c("mrnafc", "protfc")], 1,
-                               function(x) group(x[1], x[2])))
+total$group <- as.factor(apply(total[,c("mrnafc", "protfc", "protpv")], 1,
+                               function(x) group(x[1], x[2], 0, x[3])))
 
 total <- merge(total, id_table, by = "Gene", all.x = TRUE)
 dir.create("groups", showWarnings = FALSE)
@@ -110,10 +111,14 @@ for (g in levels(total$group))
     write(total[total$group==g, "TAIR"], file = paste0("groups/", g, ".txt"))
 }
 
-total <- total[ abs(total$mrnafc) > TH |
-                abs(total$protfc) > TH,]
+#total <- total[ abs(total$mrnafc) > TH |
+#                abs(total$protfc) > TH,]
 
 #plot(total$mrnafc, total$protfc, pch = 19, col = total$group)
 p <- ggplot(total, aes(x=protfc, y=mrnafc, colour = group))
-p <- p + geom_point(size  = 2)
+p <- p + geom_point(size  = 3)
 print(p)
+
+#backgrpond
+
+
